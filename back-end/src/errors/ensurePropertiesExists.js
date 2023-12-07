d9cdf93d6993fcf738acd0ea348109e5cf4be0a7
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-12-07 17:21:24 -0600

    update comments

--- back-end/src/errors/ensurePropertiesExists.js
+++ back-end/src/errors/ensurePropertiesExists.js
@@ -3,9 +3,9 @@
 function ensurePropertiesExists(...properties) {
     return function (req, res, next) {
         const { data = {} } = req.body;
- // try to check that each required property exists in the data object
+
     try {
-// loop through each required property and check if it's missing from the data object
+
         properties.forEach((property) => {
             if (!data[property]) {
             const error = new Error(`A '${property}' property is required.`);
