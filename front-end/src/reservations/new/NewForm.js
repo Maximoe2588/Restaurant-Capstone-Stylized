a5b5887b318759d2f28eaf1651c49ae45af2ca3c
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2024-01-05 06:51:24 -0600

    update styling new reservation

--- front-end/src/reservations/new/NewForm.js
+++ front-end/src/reservations/new/NewForm.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Form from "../form/Form";
-
+import './newForm.css';
 
 
 function newForm() {
     return (
+        <div className="new-res-container">
         <section>
+            
             <div className="d-md-flex mb-3">
                 <h1 className="mb-0 text-center">New Reservation</h1>
             </div>
                 <Form method={"POST"} />
+            
         </section>
+        </div>
     );
 }
 
